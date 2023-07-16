1e14cdda819a69f54edf77ec836c28eac6f99cbd
shama <assiddik001@gmail.com>
2023-07-16 22:02:30 +0600

    issue fix

--- src/sections/Books.tsx
+++ src/sections/Books.tsx
@@ -61,6 +61,8 @@ export const Books = () => {
     return <Loading />;
   }
 
+  const slicedBook = books?.length > 10 ? books?.slice(0, 10) : books?.slice(0);
+
   return (
     <div className="p-[20px]">
       {/* Section Header */}
@@ -74,74 +76,70 @@ export const Books = () => {
       {/* Section Body */}
       <div className="container md:py-[30px] pb-[30px]  mx-auto">
         <div className="flex flex-wrap -m-4">
-          {books
-            .slice(0, 10)
-            ?.map(
-              (book: {
-                _id: string | undefined;
-                bookImgUrl: string | undefined;
-                title: string | undefined;
-                genre: string | undefined;
-                author: string | undefined;
-                publishedDate: string | undefined;
-              }) => (
-                <div key={book._id} className="p-4 w-full md:w-1/2">
-                  <div className="card md:min-h-[300px] md:items-center relative lg:card-side bg-base-100 box_shadow p-2">
-                    <button
-                      onClick={() =>
-                        addReadlist({ data: { bookId: book._id } })
-                      }
-                      className="second_button duration-300 rounded-full py-[4px] px-[6px] font-medium absolute right-2 top-2 text-sm"
-                    >
-                      + Reading
-                    </button>
-                    <figure className="md:w-[40%]">
-                      <img
-                        className="object-contain m-auto h-[200px] w-[200px]  "
-                        src={book?.bookImgUrl}
-                        alt="Album"
-                      />
-                    </figure>
-                    <div className="md:w-[60%] card-body md:flex justify-center">
-                      <h2 className="card-title capitalize ">{book?.title}</h2>
-                      <ul className="info_list">
-                        <li className="info">
-                          {" "}
-                          <span className="font-bold">Genre: </span>
-                          {book?.genre}
-                        </li>
-                        <li className="info">
-                          <span className="font-bold">Author: </span>
-                          {book?.author}
-                        </li>
-                        <li className="info">
-                          <span className="font-bold">Published At: </span>
-                          {book?.publishedDate}
-                        </li>
-                      </ul>
-                      <div className="card-actions">
-                        <div className="flex justify-center gap-[15px] mt-[20px]">
-                          <button
-                            onClick={() =>
-                              addWishlist({ data: { bookId: book._id } })
-                            }
-                            className="second_button duration-300 rounded-full py-[8px] px-[12px] font-medium "
-                          >
-                            Wishlist
-                          </button>
-                          <button
-                            onClick={() => navigate(`/books/${book?._id}`)}
-                            className="first_button duration-300 rounded-full py-[8px] px-[12px] font-medium "
-                          >
-                            Show Details
-                          </button>
-                        </div>
+          {slicedBook?.map(
+            (book: {
+              _id: string | undefined;
+              bookImgUrl: string | undefined;
+              title: string | undefined;
+              genre: string | undefined;
+              author: string | undefined;
+              publishedDate: string | undefined;
+            }) => (
+              <div key={book._id} className="p-4 w-full md:w-1/2">
+                <div className="card md:min-h-[300px] md:items-center relative lg:card-side bg-base-100 box_shadow p-2">
+                  <button
+                    onClick={() => addReadlist({ data: { bookId: book._id } })}
+                    className="second_button duration-300 rounded-full py-[4px] px-[6px] font-medium absolute right-2 top-2 text-sm"
+                  >
+                    + Reading
+                  </button>
+                  <figure className="md:w-[40%]">
+                    <img
+                      className="object-contain m-auto h-[200px] w-[200px]  "
+                      src={book?.bookImgUrl}
+                      alt="Album"
+                    />
+                  </figure>
+                  <div className="md:w-[60%] card-body md:flex justify-center">
+                    <h2 className="card-title capitalize ">{book?.title}</h2>
+                    <ul className="info_list">
+                      <li className="info">
+                        {" "}
+                        <span className="font-bold">Genre: </span>
+                        {book?.genre}
+                      </li>
+                      <li className="info">
+                        <span className="font-bold">Author: </span>
+                        {book?.author}
+                      </li>
+                      <li className="info">
+                        <span className="font-bold">Published At: </span>
+                        {book?.publishedDate}
+                      </li>
+                    </ul>
+                    <div className="card-actions">
+                      <div className="flex justify-center gap-[15px] mt-[20px]">
+                        <button
+                          onClick={() =>
+                            addWishlist({ data: { bookId: book._id } })
+                          }
+                          className="second_button duration-300 rounded-full py-[8px] px-[12px] font-medium "
+                        >
+                          Wishlist
+                        </button>
+                        <button
+                          onClick={() => navigate(`/books/${book?._id}`)}
+                          className="first_button duration-300 rounded-full py-[8px] px-[12px] font-medium "
+                        >
+                          Show Details
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-              )
-            )}
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
